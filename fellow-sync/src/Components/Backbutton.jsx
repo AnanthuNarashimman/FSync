7c313c7ebd8a1b4ed795cea8e2d68f5e9c58fda9
AnanthuNarashimman <ananthu.narashimman@gmail.com>
2025-04-06 16:04:12 +0530

    vercel naming confilct resolve - 3

--- fellow-sync/src/Components/Backbutton.jsx
+++ fellow-sync/src/Components/Backbutton.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
-import '../styles/componentstyles/backbutton.css';
+import '../styles/componentstyles/Backbutton.css';
 
 function Backbutton() {
 
